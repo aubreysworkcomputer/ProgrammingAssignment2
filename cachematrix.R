611fee64fc880d6ffeb6e53575a2c47dfa893b57
aubreysworkcomputer <aubreysworkcomputer@gmail.com>
2015-09-24 12:26:23 -0500

    still giving error $ not allowed for atomic vectors

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a matrix, and recall the cache or calculate it
 
-## Write a short comment describing this function
+## This is where we take a matrix and find and cache it's inverse using the solve() function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+set <- function(y) {
+  x <<- y
+  m <<- NULL
+}
+get <- function() x
+setinverse <- function(solve) m <<- solve
+getinverse <- function() m
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function also finds the inverse of a matrix, using an if statement to determine if a cached
+## value for inverse exists and if so utilizes this value skipping actual calculations, and if not,
+## will calculate the inverse using the solve() function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+m <- x$getinverse()
+if(!is.null(m)) {
+  message("getting cached data")
+  return(m)
+}
+data <- x$get()
+m <- solve(data, ...)
+x$setinverse(m)
+m
 }
